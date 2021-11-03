8bcd4eedc5473855c453b619967dc0953f1030d7
VibeValley <xgujoh1999@gmail.com>
2021-11-03 18:26:52 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import './App.css';
 function App() {
   return (
     <body>
+
+<p>Hejsan jag heter</p>
     <div className="App">
 
       <div id="Pick_me_button" className="Button_first_page">
 
       </div>
       <div id="Alla_spel_button" className="Button_first_page">
-        <p>Hejsan jag heter</p>
+        
       </div>
     </div>
     </body>
