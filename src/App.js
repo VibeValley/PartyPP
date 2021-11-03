78a35595136d186452d0f19bfb585e4b2b5695a2
EkbergarN <andreas0111@live.se>
2021-11-03 18:29:47 +0100

    Hej

--- src/App.js
+++ src/App.js
@@ -3,9 +3,17 @@ import './App.css';
 
 function App() {
   return (
+    <body>
     <div className="App">
-      <p>Hej då min vän</p>
+
+      <div id="Pick_me_button" className="Button_first_page">
+
+      </div>
+      <div id="Alla_spel_button" className="Button_first_page">
+
+      </div>
     </div>
+    </body>
   );
 }
 
