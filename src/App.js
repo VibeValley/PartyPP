2f307e98415f9262092a449c60075a46ca4ee37e
EkbergarN <andreas0111@live.se>
2021-11-04 12:28:29 +0100

    Ny grej

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
 
-function App() {
+const FirstPageButton = ({ Input }) => {
   return (
-      <div className="App">
-        <h2 className="First_page_title">Party Portalen</h2>
-        <div className="Button_div">
-          <button class="Button_first_page" id="Pick_me_button"><p className="First_page_buttontext">Slumpa spel</p></button>
-          <button class="Button_first_page" id="Alla_spel_button"><p class="First_page_buttontext">Alla Spel</p></button>
-
-        </div>
+      <div className="Button_div">
+        <button class="Button_first_page" id="Pick_me_button"><p className="First_page_buttontext">Slumpa spel</p></button>
+        <button class="Button_first_page" id="Alla_spel_button"><p class="First_page_buttontext">Alla Spel</p></button>
 
       </div>
+      );
+}
+function App() {
+  return (
+    <div className="App">
+    <h2 className="First_page_title">Party Portalen</h2>
+   <FirstPageButton></FirstPageButton>
+   </div>
   );
 }
 
