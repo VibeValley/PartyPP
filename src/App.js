241dc39a7fded4fda20891403bc140a3cd6cc543
EkbergarN <andreas0111@live.se>
2021-11-04 13:07:39 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,47 @@
 import './App.css';
 import {
   BrowserRouter as Router,
-  Switch,
+  Routes as Switch,
   Route,
-  Link,
+  Link, 
 } from "react-router-dom";
+
 
 const FirstPageButton = ({ Input }) => {
   return (
-      <div className="Button_div">
-        <button class="Button_first_page" id="Pick_me_button"><p className="First_page_buttontext">Slumpa spel</p></button>
-        <button class="Button_first_page" id="Alla_spel_button"><p class="First_page_buttontext">Alla Spel</p></button>
-
-      </div>
-      );
+    <div className="Button_div">
+      <button class="Button_first_page" id="Pick_me_button"><p className="First_page_buttontext">Slumpa spel</p></button>
+      <button class="Button_first_page" id="Alla_spel_button"><p class="First_page_buttontext">Alla Spel</p></button>
+    </div>
+  );
 }
 function App() {
   return (
-    <div className="App">
-    <h2 className="First_page_title">Party Portalen</h2>
-   <FirstPageButton></FirstPageButton>
-   </div>
+    <div>
+      <Router>
+        <Switch>
+         <Route path="/AllaSpel" element={<AllaSpel/>} />
+          <Route path="/" element={<MainPage/>}/>
+        </Switch>
+      </Router>
+    </div>
   );
+}
+
+function MainPage() {
+  return (
+    <div className="App">
+      <Link to="/AllaSpel">Back to List</Link>
+      <h2 className="First_page_title">Party Portalen</h2>
+      <FirstPageButton />
+    </div>
+  );
+}
+
+function AllaSpel() {
+return(
+  <h2>HEj</h2>
+)
 }
 
 export default App;
