7e4410e28dd14b6ab1b802dd6f6835de9c6d2e6b
EkbergarN <andreas0111@live.se>
2021-11-04 13:58:14 +0100

    Version 1.1

--- src/App.js
+++ src/App.js
@@ -7,11 +7,10 @@ import {
 } from "react-router-dom";
 
 
-const FirstPageButton = ({ Input }) => {
+const FirstPageButton = ({ButtonText}) => {
   return (
     <div className="Button_div">
-      <button class="Button_first_page" id="Pick_me_button"><p className="First_page_buttontext">Slumpa spel</p></button>
-      <button class="Button_first_page" id="Alla_spel_button"><p class="First_page_buttontext">Alla Spel</p></button>
+      <button  className="Button_first_page" id="Pick_me_button"><p className="First_page_buttontext">{ButtonText}</p></button>
     </div>
   );
 }
@@ -31,10 +30,12 @@ function App() {
 function MainPage() {
   return (
     <div className="App">
-      <Link to="/AllaSpel">Back to List</Link>
       <h2 className="First_page_title">Party Portalen</h2>
-      <FirstPageButton />
-    </div>
+      <FirstPageButton ButtonText={"Slumpa Spel"}/>
+      <Link to="/AllaSpel">
+      <FirstPageButton ButtonText={"Alla Spel"}/>
+      </Link>
+      </div>
   );
 }
 
