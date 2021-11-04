ca23a2e2d3a679e8f8ebff4bce227dbf578522e4
EkbergarN <andreas0111@live.se>
2021-11-04 10:07:11 +0100

    Ny design
    En början

--- src/App.js
+++ src/App.js
@@ -3,17 +3,15 @@ import './App.css';
 
 function App() {
   return (
-    <body>
-    <div className="App">
+      <div className="App">
+        <h2 className="First_page_title">Party Portalen</h2>
+        <div className="Button_div">
+          <button class="Button_first_page" id="Pick_me_button"><p className="First_page_buttontext">Slumpa spel</p></button>
+          <button class="Button_first_page" id="Alla_spel_button"><p class="First_page_buttontext">Alla Spel</p></button>
 
-      <div id="Pick_me_button" className="Button_first_page">
+        </div>
 
       </div>
-      <div id="Alla_spel_button" className="Button_first_page">
-
-      </div>
-    </div>
-    </body>
   );
 }
 
