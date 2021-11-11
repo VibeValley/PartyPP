f46e52e2cab64f4ac8aea05a2bffa94564f558b6
greatrex420 <77015838+greatrex420@users.noreply.github.com>
2021-11-11 14:48:12 +0100

    lade till search i allaspel.js
    laddade också ner fuse!!!!!!!

--- src/AllaSpel.js
+++ src/AllaSpel.js
@@ -2,20 +2,52 @@ import "./App.css";
 import SpelLista from "./SpelLista.js";
 import games from "./games.json";
 import Lowerbar from "./LowerBar";
+import Fuse from 'fuse.js';
+import React, { useState } from "react";
+
 
 //Alla spel sidan
 function AllaSpel() {
   const Lista_med_spel = games;
+
+  const [query, setQuery] = useState('');
+
+  const fuse = new Fuse(games, { 
+    keys: [
+      'name',
+      'beskrivning',
+      'rules',
+      'spelare'
+    ],
+    includeScore : true
+  })
+
+  const results = fuse.search(query);
+  const gamesResult = query ? results.map(results => results.item) : games;
+
+  function handleOnSearch({currentTarget= {} }){
+    const {value} = currentTarget;
+    setQuery(value);
+  }
+
+
   return (
     <div className="App">
       {/*Det som står i rutan högst upp på sidan*/}
       <div className="HeaderDiv">
         <header className="SidaHeader">Alla Spel</header>
       </div>
+      <form>
+        <label>
+         Search:
+          <input type="text" name="name" value = {query} onChange={handleOnSearch} />
+       </label>
+          <input type="submit" value="Search" />
+      </form>
       <div className="ListaDiv">
         {/*Det som skriver ut hela listan med spel, 
         för att bestämma vad den ska skriva ut tiita i "SpelLista.js"*/}
-        {Lista_med_spel.map((allaSpel) => (
+        {gamesResult.map((allaSpel) => (
           <SpelLista key={allaSpel.id} spel={allaSpel} />
         ))}
       </div>
