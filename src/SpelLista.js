f99634d6afbe0dea2afbbb38955bf40decb1b3df
Luckelina <lyckos@live.se>
2021-11-18 13:01:51 +0100

    första edit

--- src/SpelLista.js
+++ src/SpelLista.js
@@ -4,10 +4,20 @@ const SpelLista = ({ spel }) => {
   // Detta är hur den skriver ut hela listan
   return (
     <Link to={"/AllaSpel/" + spel.id}>
-      <div className="Alla_spel_listans_text">
-        <h1>{spel.name}</h1>
-        <p>{spel.beskrivning}</p>
-        <p>{spel.spelare}</p>
+      <div className="Alla_spel_listan">
+        <div className="Alla_spel_titel">
+          <h1>{spel.name}</h1>
+        </div>
+        <div ClassName="Alla_spel_Beskrivning">
+          <p>{spel.beskrivning}</p>
+        </div>
+        <div ClassName="Alla_spel_Spelare">
+          <p>{spel.spelare}</p>
+        </div>
+        <div ClassName="Alla_spel_Material">
+          <p>(Ikoner här)</p>
+        </div>
+        
       </div>
     </Link>
   );
