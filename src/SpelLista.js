b78db8f88944a48eaea72b971f6e77872e691980
EkbergarN <andreas0111@live.se>
2021-12-01 15:45:13 +0100

    pog

--- src/SpelLista.js
+++ src/SpelLista.js
@@ -4,19 +4,7 @@ import { Link } from "react-router-dom";
 import emptyStar from "./Bilder/FavvoSymbol.png";
 import filledStar from "./Bilder/FavvoSymbolFylld.png";
 
-// const changeImage = (id, name) => {
-//   if (document.getElementById(id).src.match({ emptyStar })) {
-//     document.getElementById(id).src = { filledStar };
-//     window.localStorage.setItem(id, JSON.stringify(name));
-//   } else if (document.getElementById(id).src.match({ filledStar })) {
-//     document.getElementById(id).src = { emptyStar };
-//     localStorage.removeItem(id);
-//   }
-// };
-
 const SpelLista = ({ spel }) => {
-  //const [favorit, väljFavorit] = useState("");
-  // Detta är hur den skriver ut hela listan
   const [isFavorite, setIsFavorit] = useState(
     localStorage.getItem(spel.id) !== null
   );
@@ -29,15 +17,13 @@ const SpelLista = ({ spel }) => {
 
   return (
     <div className="Alla_spel_listans_text">
-      <div>
-        <img
-          src={isFavorite ? filledStar : emptyStar}
-          onClick={() => addFavorit(spel.id, spel.namn)}
-          alt="AllaSpel"
-          className="favBox"
-          id={spel.id}
-        ></img>
-      </div>
+      <img
+        src={isFavorite ? filledStar : emptyStar}
+        onClick={() => addFavorit(spel.id, spel.namn)}
+        alt="AllaSpel"
+        className="favBox"
+        id={spel.id}
+      ></img>
       <Link to={"/AllaSpel/" + spel.id}>
         <div>
           <h1 className="ListaTitel">{spel.namn}</h1>
