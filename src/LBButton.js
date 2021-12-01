cb3c086586eba1eb67303bacc5378671f7a28dec
VibeValley <xgujoh1999@gmail.com>
2021-12-01 15:56:54 +0100

    Update LBButton.js

--- src/LBButton.js
+++ src/LBButton.js
@@ -5,18 +5,16 @@ import { Link } from "react-router-dom";
 const LBButton = ({img, text, linkPath, buttonID, SelectedID}) => {
 
 
-let tempColor = SelectedID === buttonID ? '#ffd005' : 'linear-gradient(180deg, #ffb800 -37.5%, #f6e441 50%)';
+let tempColor = SelectedID === buttonID ? '#f5bd22' : 'linear-gradient(180deg, #ffb800 -37.5%, #f6e441 50%)';
 
 
 
 return(
         <Link to={linkPath}>
-            
           <button style={{ background: tempColor }} id={buttonID}  className="knappar_bot" type="button">
             <img src={img} alt="AllaSpel" className="FPic"></img>
             <p  className="knappar_text">{text}</p>
           </button>
-          
         </Link>
 );
 
